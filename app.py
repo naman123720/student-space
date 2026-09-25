f17a8526f4479a8155a7be754f4c26d185101e2a
naman123720 <26BEL10019@cuchd.in>
2026-09-25 17:37:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ elif option == "📁 My Files":
     try:
         files = supabase.storage.from_("student-files").list()
 
-       if files:
+       if files :
     for file in files:
         file_name = file["name"]
         st.write(f"📄 {file_name}")
