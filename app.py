c71fba2157e913999b571880860457eef58324a1
naman123720 <26BEL10019@cuchd.in>
2026-09-25 16:56:10 +0530

    Fix variable assignment for SUPABASE_KEY

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from supabase import create_client
 SUPABASE_URL=st.secrets["SUPABASE_URL"]
-SUPABASE_URL=st.secrets["SUPABASE_KEY"]
+SUPABASE_KEY=st.secrets["SUPABASE_KEY"]
 supabase= create_client(SUPABASE_URL, SUPABASE_KEY)
 st.set_page_config(
     page_title="Student Space",
